2b4770a4f1c131d47c6c98e1c75204d7945d5020
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-08 09:14:26 +0200

    return await

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-var module = require('./contacts.js');
-// module.listContacts(); // запуск экспортируемой функции модуля
-// module.getContactById(5);
-// module.addContact('andrew', 'kova@mail.ru', '(594) 840-65585');
+const { listContacts, getContactById, addContact, removeContact } = require('./contacts.js');
 // module.addContact(
 //     'hhhhhhhhhh',
 //     'mailr',
 //     '555555554'
 // );
-// module.removeContact(a72c40c6);
 const argv = require('yargs').argv;
 
-function invokeAction({ action, id, name, email, phone }) {
+const invokeAction = async({ action, id, name, email, phone })=> {
   switch (action) {
     case 'list':
-      module.listContacts();
-      break;
+      return await listContacts();
 
     case 'get':
-      module.getContactById(id);
-      break;
+      return await getContactById(id);
 
     case 'add':
-      module.addContact(name, email, phone);
-      break;
+      return await addContact(name, email, phone);
 
     case 'remove':
-      module.removeContact(id);
-      break;
+      return await removeContact(id);
 
     default:
       console.warn('\x1B[31m Unknown action type!');
