1f738d7147d5171bbda8a69d1b47e4ba927cca02
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-12-08 09:02:26 +0200

    yargs

--- index.js
+++ index.js
@@ -7,4 +7,30 @@ var module = require('./contacts.js');
 //     'mailr',
 //     '555555554'
 // );
-module.removeContact('a72c40c6-6125-48f2-8a59-68327db4d0ca');
+// module.removeContact(a72c40c6);
+const argv = require('yargs').argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      module.listContacts();
+      break;
+
+    case 'get':
+      module.getContactById(id);
+      break;
+
+    case 'add':
+      module.addContact(name, email, phone);
+      break;
+
+    case 'remove':
+      module.removeContact(id);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
